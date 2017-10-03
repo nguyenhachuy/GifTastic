a1116556cc9a986082b3f51d3e42b8abb3abdab0
nguyenhachuy <nguyenhachuy@gmail.com>
2017-10-03 16:52:06 -0700

    More functionlity

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,15 +7,37 @@ $(document).ready(function() {
 	display();
 });
 
+//Dynamic Jquery 
+$(document).on("click", ".button", ajaxCall);
 //Global vars
+var queryURL = "https://api.giphy.com/v1/gifs/search";
+var key = "dc6zaTOxFJmzC";
+
 var topics = ["Rick Sanchez", "Morty Smith", "Betty Smith", "Summer Smith"];
 var buttonClass = "btn btn-success button mx-auto";
 function display() {
 	topics.forEach((value, index, array) => {
 		//Make a button
-		var button = $("<button>").addClass(buttonClass).text(value);
+		var button = $("<button>").addClass(buttonClass).text(value).data("topic", value);
 		//Append to row
 		$(".button-row").append(button);
 	});
 }
 
+function ajaxCall() {
+	var topic = $(this).data("topic");
+	$.ajax({
+		url: queryURL,
+		method:"GET",
+		data: {
+			api_key: key,
+			q: topic,
+			rating: "g"
+		},
+	}).done(function(response) {
+		console.log(response);
+		var gif = $("<img>");
+		gif.attr("src", response.data[0].images.fixed_height.url);
+		$(".row").append(gif);
+	});
+}
